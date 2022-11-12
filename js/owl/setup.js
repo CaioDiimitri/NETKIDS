6929c15db32b716da6070cf6e9dde6a93e77cfce
dimitri <caiodemitri@gmail.com>
2022-11-12 08:53:36 -0300

    arquivos js

---
+++ js/owl/setup.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+function linkjogos(){
+    window.open ("https://www.escolagames.com.br/");
+
+}
+
